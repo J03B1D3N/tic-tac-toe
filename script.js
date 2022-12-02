027913b2f23506593b18715a772a2d2fe3d50092
Justas Lapiene <Justas63@gmail.com>
2022-12-02 14:53:29 +0100

    cashed the dom, now working on the x and O interchangeability functions

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
-const Gameboard = function() {
-    let gameboard = []
-}
+(function() {
 
-const Players = function() {
+    const GAMEBOARD = {
+        
+        cacheDom: function () {
+            this.cell = document.querySelectorAll(".cell");
+            this.board = document.getElementById('board');
+            this.winningMessage = document.getElementById('winningMessage');
+            this.winningMessageText = document.querySelectorAll('data-winning-message-text')
+            this.restartButton = document.getElementById('restartButton');
+        },
+        
+        
+        playerCircleTurn: function () {
+            this.board.setAttribute('class', 'board circle');
+
+            for(i = 0; i < this.cell.length; i++) {
+                this.cell[i].addEventListener('click', function(e) {
+                    e.target.setAttribute('class', 'cell circle')
+                })
+            }
+        },
+        playerXTurn: function () {
+            this.board.setAttribute('class', 'board x');
+
+            for(i = 0; i < this.cell.length; i++) {
+                this.cell[i].addEventListener('click', function(e) {
+                    e.target.setAttribute('class', 'cell x'),
+                    playerCircleTurn();
+                    
+                })
+            }
+        },
+        game: function() {
+            
+        }
+       
+        
+    }   
+    GAMEBOARD.cacheDom();
+    GAMEBOARD.playerXTurn();
     
-}
-
-const Game = function() {
-    let game = []
-}
+    
+})()
