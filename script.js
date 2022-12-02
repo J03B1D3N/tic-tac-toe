890ccb69d037ac0e3f62e4b356e0761981d21913
Justas Lapiene <Justas63@gmail.com>
2022-12-02 20:04:17 +0100

    added x & O placement functionality. trying to get checkWiner() to work

--- script.js
+++ script.js
@@ -1,44 +1,68 @@
 (function() {
+   const X_CLASS = 'x'
+   const CIRCLE_CLASS = 'circle'
+   const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+   ];
+   const cellElement = document.querySelectorAll("[data-cell]");
+   const board = document.getElementById('board');
+   const winningMessageElement = document.getElementById('winningMessage');
+   const winningMessageText = document.querySelector('data-winning-message-text')
+   const restartButton = document.getElementById('restartButton');
+   let circleTurn
+   
+   startGame();
 
-    const GAMEBOARD = {
-        
-        cacheDom: function () {
-            this.cell = document.querySelectorAll(".cell");
-            this.board = document.getElementById('board');
-            this.winningMessage = document.getElementById('winningMessage');
-            this.winningMessageText = document.querySelectorAll('data-winning-message-text')
-            this.restartButton = document.getElementById('restartButton');
-        },
-        
-        
-        playerCircleTurn: function () {
-            this.board.setAttribute('class', 'board circle');
+   
 
-            for(i = 0; i < this.cell.length; i++) {
-                this.cell[i].addEventListener('click', function(e) {
-                    e.target.setAttribute('class', 'cell circle')
-                })
-            }
-        },
-        playerXTurn: function () {
-            this.board.setAttribute('class', 'board x');
-
-            for(i = 0; i < this.cell.length; i++) {
-                this.cell[i].addEventListener('click', function(e) {
-                    e.target.setAttribute('class', 'cell x'),
-                    playerCircleTurn();
-                    
-                })
-            }
-        },
-        game: function() {
-            
+   function startGame() {
+    circleTurn = false
+        cellElement.forEach(cell => {cell.addEventListener('click', handleClick, {once:true})})
+    setBoardHoverClass();
+   }
+    
+   function handleClick(e) {
+        const cell = e.target
+        const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+        if(checkWin(currentClass)){
+            console.log('winner')
         }
+
+        placeMark(cell, currentClass)
+        swapTurns()
+        setBoardHoverClass()
+    }
+    function placeMark(cell, currentClass) {
+        cell.classList.add(currentClass)
+    }
+    function swapTurns() {
+        circleTurn = !circleTurn  
+    }
+    function setBoardHoverClass() {
+        board.classList.remove(X_CLASS)
+        board.classList.remove(CIRCLE_CLASS)
+        if(circleTurn) {
+            board.classList.add(CIRCLE_CLASS)
+        } else {
+            board.classList.add(X_CLASS)
+        }
+    }
+
+    function checkWin(currentClass) {
+        return WINNING_COMBINATIONS.some(combination => {
+          return combination.every(index => {
+            return cellElement[index].classList.contains(currentClass)
+          })
+        })
+      }
+
        
         
-    }   
-    GAMEBOARD.cacheDom();
-    GAMEBOARD.playerXTurn();
-    
-    
-})()
+    })()
